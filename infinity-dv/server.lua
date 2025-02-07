f7a8d33db8e1235fd77064d196834ebe2515f90c
yuhdean <deannobusiness@gmail.com>
2025-02-06 21:19:09 -0500

    Update server.lua

--- infinity-dv/server.lua
+++ infinity-dv/server.lua
@@ -1,8 +1,8 @@
 RegisterNetEvent('deletevehicle:logToDiscord')
 AddEventHandler('deletevehicle:logToDiscord', function(vehicleName)
-    local webhookUrl = 'https://discord.com/api/webhooks/1336448357249323018/b_uxNRIdyIrxKse4fpHpCvJ0WFCMRcu3gyurVOzBNb0-kbLVsmSWAXa2CzuHjUsePUrF' -- Replace with your Discord webhook
+    local webhookUrl = 'YOUR_WEBHOOK_GOES_HERE' 
 
-    -- Log the vehicle deletion to Discord
+
     PerformHttpRequest(webhookUrl, function(err, text, headers) end, 'POST', json.encode({
         username = "Vehicle Deletion",
         content = "",
