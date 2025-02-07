a01ce34537824e8f76e4b733bf82b5c4c8345b34
yuhdean <deannobusiness@gmail.com>
2025-02-07 15:56:50 -0500

    Update fxmanifest.lua

--- infinity-antishuffle/fxmanifest.lua
+++ infinity-antishuffle/fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Infinity Development'
-description 'Prevents passengers from shuffling seats'
-version '1.0.0'
+author 'Dean'
+description ''
+version ''
 
 client_script 'client.lua'
