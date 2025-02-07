9457f01871dbc3fa208150179af51dcc6d3640b3
yuhdean <deannobusiness@gmail.com>
2025-02-06 21:19:50 -0500

    Update client.lua

--- infinity-antishuffle/client.lua
+++ infinity-antishuffle/client.lua
@@ -1,6 +1,5 @@
 local disallowed = false
 
--- Call the function from functions.lua to handle seat shuffling prevention
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
@@ -8,11 +7,9 @@ Citizen.CreateThread(function()
         local playerPed = PlayerPedId()
         local vehicle = GetVehiclePedIsIn(playerPed, false)
 
-        -- Check if the player is in a vehicle and is not the driver
         if vehicle ~= 0 and GetPedInVehicleSeat(vehicle, -1) ~= playerPed then
             local seatIndex = GetVehiclePedIsTryingToEnter(playerPed)
 
-            -- If the player is trying to swap seats, cancel the action
             if seatIndex ~= -1 and seatIndex ~= 0 then
                 disallowed = true
                 ClearPedTasksImmediately(playerPed)
